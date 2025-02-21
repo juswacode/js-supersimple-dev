3b233e57c646f15ccfa8a5652994a3166ecf28db
juswacode <reyesjoshuagil@gmail.com>
2025-02-21 17:30:18 +0800

    Lesson 11, challenges and exercises | Try to commit any each day just for the sake of being consistent in coding each day.

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,7 @@
-const todoList = [];
+const todoList = [{
+  name: 'Read scriptures',
+  dueDate: '2022-10-2'
+}];
 
 //To show what is inside the list.
 renderTodoList();
@@ -8,8 +11,18 @@ function renderTodoList () {
   let todoListHTML = '';
 
   for (let i=0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      const html = `<p>${todo}</p>`;
+      const todoObject = todoList[i];
+      const name = todoObject.name;
+      const dueDate = todoObject.dueDate;
+
+      const html = `<p>
+                      ${name} ${dueDate}
+                      <button onclick="
+                      todoList.splice(${i}, 1);
+                      renderTodoList();
+                      ">Delete</button>
+                    </p>`;
+
       todoListHTML += html;
   
       
@@ -23,12 +36,15 @@ function renderTodoList () {
 
 function addToDo() {
   const inputElement = document.querySelector('.js-name-input');
+  const dateInputElement = document.querySelector('.js-date-input');
 
   // get the value from the INPUT variable
   const name = inputElement.value;
+  const dueDate = dateInputElement.value;
 
-  //add the value or data that is entered into Array. 
-  todoList.push(name);
+  //add the value or data that is entered into Object Array. 
+  todoList.push({name, dueDate
+  });
 
   //reset the text inside the INPUT table
   inputElement.value = '';
